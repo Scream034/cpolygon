c4a9d840b8299cc19148857c2ff04a0c50fe138f
paralax <kostgs034@gmail.com>
2025-04-07 12:28:21 +0500

    Its bad

--- classes/c_geometry_2d.gd
+++
@@ -1,9 +0,0 @@
-extends Object
-class_name CGeometry2D
-
-## X = A - B
-static func intersection(polygon_a: PackedVector2Array, polygon_b: PackedVector2Array) -> Array[PackedVector2Array]:
-	var result: Array[PackedVector2Array] = Geometry2D.clip_polygons(polygon_a, polygon_b)
-	if result.is_empty(): return []
-
-	return result
